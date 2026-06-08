64d28a10ea736f5dd5add9bca3b8c15de007a634
Sammy Shear <sammyshear1@gmail.com>
2026-06-08 16:18:56 -0400

    markdown stuff mostly

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -46,8 +46,11 @@ return {
       opts = {
         linters = {
           ["markdownlint-cli2"] = {
-            args = { "--config", vim.fn.expand("~") .. "/markdownlint.yaml", "--" },
+            args = { "--config", vim.fn.expand("~") .. "/.config/nvim/markdownlint.json", "--" },
           },
+        },
+        linters_by_ft = {
+          markdown = { "markdownlint-cli2" },
         },
       },
     },
@@ -71,9 +74,29 @@ return {
   {
     "stevearc/conform.nvim",
     opts = {
+      formatters = {
+        ["markdown-toc"] = {
+          condition = function(_, ctx)
+            for _, line in ipairs(vim.api.nvim_buf_get_lines(ctx.buf, 0, -1, false)) do
+              if line:find("<!%-%- toc %-%->") then
+                return true
+              end
+            end
+          end,
+        },
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fn.expand("~") .. "/.config/nvim/markdownlint.json", "--" },
+          condition = function(_, ctx)
+            local diag = vim.tbl_filter(function(d)
+              return d.source == "markdownlint"
+            end, vim.diagnostic.get(ctx.buf))
+            return #diag > 0
+          end,
+        },
+      },
       formatters_by_ft = {
-        templ = { "templ" },
-        markdown = { "mdsf", "mdslw", "markdownlint-cli2", "markdown-toc" },
+        ["markdown"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
+        ["markdown.mdx"] = { "prettier", "markdownlint-cli2", "markdown-toc" },
       },
     },
   },
