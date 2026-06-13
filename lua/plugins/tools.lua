9016c6ed8f376ab96d093d6fccbf2ebc0e9ea153
Sammy Shear <sammyshear1@gmail.com>
2026-06-12 20:17:31 -0400

    rm neogit

--- lua/plugins/tools.lua
+++ lua/plugins/tools.lua
@@ -100,17 +100,4 @@ return {
       },
     },
   },
-  {
-    "NeogitOrg/neogit",
-    lazy = true,
-    dependencies = {
-      "sindrets/diffview.nvim",
-      "m00qek/baleia.nvim",
-      "folke/snacks.nvim",
-    },
-    cmd = "Neogit",
-    keys = {
-      { "<leader>gg", "<cmd>Neogit<cr>", desc = "Show Neogit UI" },
-    },
-  },
 }
